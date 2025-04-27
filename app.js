dc7ef8563235aa62a1d5a327da69b89313cedceb
KhaiHoang-Tran <tkhai1491@gmail.com>
2025-04-27 21:23:50 +0700

    add practiclejs and responsive website for fullscreen

---
+++ app.js
@@ -0,0 +1,26 @@
+particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 60,
+        density: { enable: true, value_area: 800 },
+      },
+      color: { value: "#fff" },
+      shape: { type: "star" },
+      opacity: { value: 1, random: true },
+      size: { value: 4, random: true },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#9932CC",
+        opacity: 0.2,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "none",
+        random: true,
+        out_mode: "out",
+      },
+    },
+  });
